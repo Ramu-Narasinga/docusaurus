15c798e2eaef0d6fb61a02b834ac77959ab87aca
sebastienlorber <lorber.sebastien@gmail.com>
2023-10-24 18:53:00 +0200

    cleanup layout

--- packages/docusaurus-theme-classic/src/theme/Layout/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/Layout/index.tsx
@@ -9,15 +9,10 @@ import React from 'react';
 import clsx from 'clsx';
 import ErrorBoundary from '@docusaurus/ErrorBoundary';
 import {
-  PageMetadata,
   SkipToContentFallbackId,
   ThemeClassNames,
 } from '@docusaurus/theme-common';
 import {useKeyboardNavigation} from '@docusaurus/theme-common/internal';
-import SkipToContent from '@theme/SkipToContent';
-import AnnouncementBar from '@theme/AnnouncementBar';
-import Navbar from '@theme/Navbar';
-import Footer from '@theme/Footer';
 import LayoutProvider from '@theme/Layout/Provider';
 import ErrorPageContent from '@theme/ErrorPageContent';
 import type {Props} from '@theme/Layout';
@@ -26,25 +21,14 @@ import styles from './styles.module.css';
 export default function Layout(props: Props): JSX.Element {
   const {
     children,
-    noFooter,
     wrapperClassName,
     // Not really layout-related, but kept for convenience/retro-compatibility
-    title,
-    description,
   } = props;
 
   useKeyboardNavigation();
 
   return (
     <LayoutProvider>
-      <PageMetadata title={title} description={description} />
-
-      <SkipToContent />
-
-      <AnnouncementBar />
-
-      <Navbar />
-
       <div
         id={SkipToContentFallbackId}
         className={clsx(
@@ -56,8 +40,6 @@ export default function Layout(props: Props): JSX.Element {
           {children}
         </ErrorBoundary>
       </div>
-
-      {!noFooter && <Footer />}
     </LayoutProvider>
   );
 }
