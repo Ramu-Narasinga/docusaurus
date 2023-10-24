dbdfb3745cbb186f7df29e396305cc99f40e9d5e
sebastienlorber <lorber.sebastien@gmail.com>
2023-10-24 19:03:12 +0200

    Revert "cleanup layout"
    This reverts commit 15c798e2eaef0d6fb61a02b834ac77959ab87aca.

--- packages/docusaurus-theme-classic/src/theme/Layout/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/Layout/index.tsx
@@ -9,10 +9,15 @@ import React from 'react';
 import clsx from 'clsx';
 import ErrorBoundary from '@docusaurus/ErrorBoundary';
 import {
+  PageMetadata,
   SkipToContentFallbackId,
   ThemeClassNames,
 } from '@docusaurus/theme-common';
 import {useKeyboardNavigation} from '@docusaurus/theme-common/internal';
+import SkipToContent from '@theme/SkipToContent';
+import AnnouncementBar from '@theme/AnnouncementBar';
+import Navbar from '@theme/Navbar';
+import Footer from '@theme/Footer';
 import LayoutProvider from '@theme/Layout/Provider';
 import ErrorPageContent from '@theme/ErrorPageContent';
 import type {Props} from '@theme/Layout';
@@ -21,14 +26,25 @@ import styles from './styles.module.css';
 export default function Layout(props: Props): JSX.Element {
   const {
     children,
+    noFooter,
     wrapperClassName,
     // Not really layout-related, but kept for convenience/retro-compatibility
+    title,
+    description,
   } = props;
 
   useKeyboardNavigation();
 
   return (
     <LayoutProvider>
+      <PageMetadata title={title} description={description} />
+
+      <SkipToContent />
+
+      <AnnouncementBar />
+
+      <Navbar />
+
       <div
         id={SkipToContentFallbackId}
         className={clsx(
@@ -40,6 +56,8 @@ export default function Layout(props: Props): JSX.Element {
           {children}
         </ErrorBoundary>
       </div>
+
+      {!noFooter && <Footer />}
     </LayoutProvider>
   );
 }
