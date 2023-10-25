b33601e80aa6615c69cf5063803d444a051218fb
sebastienlorber <lorber.sebastien@gmail.com>
2023-10-25 14:01:55 +0200

    remove more core layout

--- packages/docusaurus/src/client/App.tsx
+++ packages/docusaurus/src/client/App.tsx
@@ -6,35 +6,7 @@
  */
 
 import React from 'react';
-import '@generated/client-modules';
-
-import routes from '@generated/routes';
-import {useLocation} from '@docusaurus/router';
-import renderRoutes from '@docusaurus/renderRoutes';
-import Root from '@theme/Root';
-import normalizeLocation from './normalizeLocation';
-import {BrowserContextProvider} from './browserContext';
-import {DocusaurusContextProvider} from './docusaurusContext';
-import PendingNavigation from './PendingNavigation';
-
-// TODO, quick fix for CSS insertion order
-// eslint-disable-next-line import/order
-import ErrorBoundary from '@docusaurus/ErrorBoundary';
 
 export default function App(): JSX.Element {
-  const routeElement = renderRoutes(routes);
-  const location = useLocation();
-  return (
-    <ErrorBoundary>
-      <DocusaurusContextProvider>
-        <BrowserContextProvider>
-          <Root>
-            <PendingNavigation location={normalizeLocation(location)}>
-              {routeElement}
-            </PendingNavigation>
-          </Root>
-        </BrowserContextProvider>
-      </DocusaurusContextProvider>
-    </ErrorBoundary>
-  );
+  return <div>App!</div>;
 }
